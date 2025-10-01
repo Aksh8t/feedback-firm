0748610fb4bbc34fe31f03e237f6307f2d7aec3d
Your Name <akshat35tiwari@gmail.com>
2025-10-02 04:28:28 +0530

    Changes to Signups

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -29,6 +29,12 @@ export async function POST(request: Request) {
           { success: false, message: "Email already registered" },
           { status: 400 }
         );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
       }
     } else {
       const hashedPassword = await bcrypt.hash(password, 10);
