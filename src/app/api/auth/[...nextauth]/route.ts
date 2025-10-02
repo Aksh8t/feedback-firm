89eed5f8a731723f9f445511d0341ac3f9c91bf6
Your Name <akshat35tiwari@gmail.com>
2025-10-02 15:32:44 +0530

    Changes to Auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,6 @@
-import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/models/User";
-import { username } from "better-auth/plugins";
+import NextAuth from 'next-auth/next';
+import { authOptions } from './options';
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials: any): Promise<any> {
-        await dbConnect();
-        try {
-          const user = await UserModel.findOne({
-            $or: [
-              { email: credentials.identifier },
-              { username: credentials.identifier },
-            ],
-          });
+const handler = NextAuth(authOptions);
 
-          if (!user) {
-            throw new Error("No user found with the given email or username");
-          }
-        } catch (error: any) {
-          throw new Error(error);
-        }
-      },
-    }),
-  ],
-};
+export { handler as GET, handler as POST };
