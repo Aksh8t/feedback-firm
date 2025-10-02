ce2c4b003f9df476cb9811a562f8bc00a659c517
Your Name <akshat35tiwari@gmail.com>
2025-10-02 15:16:43 +0530

    Added auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,36 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { username } from "better-auth/plugins";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("No user found with the given email or username");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+};
