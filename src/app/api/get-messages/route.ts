a018d1ba52b09612b09dd3d404592ab8cf7d6e96
Your Name <akshat35tiwari@gmail.com>
2025-10-04 23:21:08 +0530

    completed db and auth

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -6,21 +6,39 @@ import { authOptions } from "../auth/[...nextauth]/options";
 import mongoose from "mongoose";
 
 export async function GET(request: Request) {
-    const session = await getServerSession(authOptions);
-    const user: User = session?.user as User;
-    
-    if (!session || !session.user) {
-        return Response.json({ successs: false, message: "Unauthorized" }, { status: 401 });
-    }
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
 
-    const userId = new mongoose.Types.ObjectId(user._id);
-    try {
-        const user = await UserModel.aggregate([
-            { $match: { _id: userId } },
-            { $project: { messages: 1, _id: 0 } }
-        ])
-        
-    } catch (error) {
-        
+  if (!session || !session.user) {
+    return Response.json(
+      { successs: false, message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+
+  const userId = new mongoose.Types.ObjectId(user._id);
+  try {
+    const user = await UserModel.aggregate([
+      { $match: { _id: userId } },
+      { $unwind: "$messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+    ]);
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        { success: false, message: "User Not Found" },
+        { status: 400 }
+      );
     }
+    return Response.json(
+      { success: true, messages: user[0].messages },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,36 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { Message } from "@/models/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const { username, content } = await request.json();
+  try {
+    const user = await UserModel.findById({ username });
+    if (!user) {
+      return Response.json(
+        { success: false, message: "User Not Found" },
+        { status: 400 }
+      );
+    }
+    if (!user.isAcceptingMessage) {
+      return Response.json(
+        { success: false, message: "User is not accepting messages" },
+        { status: 403 }
+      );
+    }
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+    await user.save();
+
+    return Response.json(
+      { success: true, message: "Message Sent Successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
